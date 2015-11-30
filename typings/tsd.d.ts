ea274c97c8617f937f736a1ace5e6fa9017d182b
Maayan Glikser <maayan@glikm.com>
2015-11-30 11:41:24 +0200

    Add typescript definitions for used libraries

---
+++ typings/tsd.d.ts
@@ -0,0 +1,6 @@
+
+/// <reference path="react/react.d.ts" />
+/// <reference path="uuid/uuid-base.d.ts" />
+/// <reference path="uuid/uuid-cjs.d.ts" />
+/// <reference path="uuid/uuid.d.ts" />
+/// <reference path="node/node.d.ts" />
