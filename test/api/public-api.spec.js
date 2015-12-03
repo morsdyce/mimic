2a4af9d0f9e60db88a304f96417a0bf5f0cb6fdf
Maayan Glikser <maayan@glikm.com>
2015-12-04 00:51:58 +0200

    Add API getters tests

--- test/api/public-api.spec.js
+++ test/api/public-api.spec.js
@@ -6,4 +6,19 @@ describe('api interface', () => {
     expect(API.version).toBe('0.0.1');
   });
 
+  it('should have capturedRequests property', () => {
+    expect(API.capturedRequests).toBeDefined();
+    expect(API.capturedRequests.length).toBe(0);
+  });
+
+  it('should have mockedRequests property', () => {
+    expect(API.mockedRequests).toBeDefined();
+    expect(API.mockedRequests.length).toBe(0);
+  });
+
+  it('should have scenarios property', () => {
+    expect(API.scenarios).toBeDefined();
+    expect(API.scenarios.length).toBe(0);
+  });
+
 });
