430034954c5d3aa958482a69b3dc8d8a6bb13388
Maayan Glikser <maayan@glikm.com>
2015-12-04 00:48:24 +0200

    Separate tests into separate files

---
+++ test/api/xhr-interceptor.spec.js
@@ -0,0 +1,28 @@
+import superagent from 'superagent';
+import Requests from 'api/requests';
+
+describe('xhr interceptor', () => {
+
+  it('should get 0 captured requests', () => {
+    expect(Requests.capturedRequests).toBeDefined();
+    expect(Requests.capturedRequests.length).toBe(0);
+  });
+
+  it('should get a captured request', (done) => {
+    expect(Requests.capturedRequests.length).toBe(0);
+
+    superagent.get('http://fake.com')
+      .timeout(20)
+      .end(() => {
+        expect(Requests.capturedRequests.length).toBe(1);
+
+        let request = Requests.capturedRequests[0];
+        expect(request.method).toBe('GET');
+        expect(request.url).toBe('http://fake.com');
+
+        done();
+      });
+
+  });
+
+});
