2b2265ed878c92facd7d5f0c6523ae207fcd888d
Maayan Glikser <maayan@glikm.com>
2016-07-10 18:47:56 +0300

    Test saucelabs CI integration #4

--- karma.conf.js
+++ karma.conf.js
@@ -9,11 +9,6 @@ const customLaunchers = {
     base: 'SauceLabs',
     browserName: 'chrome'
   },
-  'SL_InternetExplorer': {
-    base: 'SauceLabs',
-    browserName: 'internet explorer',
-    version: '10'
-  },
   'SL_FireFox': {
     base: 'SauceLabs',
     browserName: 'firefox',
