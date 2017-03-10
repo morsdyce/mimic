c9eb6ae1736b9676f93c4166d1f02ba0d3b0c283
ilyagelman <ilyagelman@ilyagelman.com>
2017-03-10 15:04:21 -0500

    add NativeScript support

---
+++ nativescript.js
@@ -0,0 +1,7 @@
+const appSettings = require('application-settings');
+global.appSettings = appSettings;
+
+global.isNativeScript = true;
+
+const remote = require('./dist/mimic.remote.js');
+module.exports = remote;
