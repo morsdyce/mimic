ab739d9e3a362bf21b29d50ad2475a3a6882d356
Maayan Glikser <maayan@glikm.com>
2017-03-17 17:56:52 +0200

    Polyfill fetch for nativescript

--- nativescript.js
+++ nativescript.js
@@ -1,11 +1,9 @@
+require(__dirname + '/polyfills/fetch');
+
 const appSettings = require('application-settings');
 global.appSettings = appSettings;
 
 global.isNativeScript = true;
-
-if (!global.WebSocket) {
-  global.WebSocket = require('nativescript-websockets');
-}
 
 const remote = require('./dist/mimic.remote.js');
 module.exports = remote;
